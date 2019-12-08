5860bb70af702354086448ae5f2a55dc8cb9c538
gnodarse <gabenodarse@yahoo.com>
2019-12-08 11:48:20 -0500

    Cleaned up pyramid sort

--- pyramid-sort.cpp
+++ pyramid-sort.cpp
@@ -2,16 +2,6 @@
 
 #include <cmath>
 
-//#define DIAGNOSTICS
-#ifdef DIAGNOSTICS
-
-#include <string>
-std::string debug = "";
-#include <map>
-std::map<std::string,int> g_measurements;
-#include <iostream>
-
-#endif
 
 /****************************************************************
 PYRAMID SORT*****************************************************
@@ -23,12 +13,12 @@ Pyramid sort is based on promotion sort: Build a tree of nested arrays, where th
 	{0!3} 0:{1} - An array with a 1 nested in an array associated with the element 0 - more generally, nested arrays are associated with the greatest element 
 	less than to all elements contained in the array.
 
-There is a small conceptual difference between pyramid sort and promotion sort and a few optimization differences that have been implemented here but I
-	have yet to apply to promotion sort. 
+There is a small conceptual difference between pyramid sort and promotion sort and a few optimization differences that have been implemented here that
+	have yet to be applied to promotion sort. 
 	-The conceptual difference is that the containers in pyramid sort always hold either 1 or 2 elements (thus forming
 	a tight, evenly distributed tree, or, as I am tentatively calling it, a pyramid). The reason for this is that upon evaluating promotion sort I found
-	out that the binary search evaluated on each nested array was a bottleneck. By limiting the amount of elements to 1 or 2, no binary search is required,
-	only a single comparison to the first element, and a possible comparison on the second element, and only if it exists.
+	out that the binary search evaluated on each nested array was a bottleneck. By limiting the amount of elements to 1 or 2, no binary search is required:
+	only a single comparison to the first element is required, and in cases where the comparison is >=, a comparison to the second element if it exists.
 	-In pyramid sort, each container has a nested array of all elements that fall before the first element. Promotion sort uses a worse solution to handling
 	elements that fall before any values in the parent that involves absolute minimums and duplicating elements.
 	-Checks for promotion happen once the bottom nest level is reached rather than at each nest level while searching down. This means that a promotion
@@ -54,24 +44,7 @@ Further possible optimizations:
 *****************************************************************/
 
 
-//TODO:
-//Find if there's a way to remove the while loop in findInsertLoc(), (while(destination->nestedContainer != nullptr))
-//Move binary search to its own function
-
-#include "nested-array-sort.h"
-
-#ifdef DIAGNOSTICS
-void printStructure(elementContainer* source, std::string containerIdentifier = "P");
-#endif
-
-elementContainer* promote(elementContainer* parent, int promotedValue, elementContainer*& superParent, int level,
-							elementContainer** containerAssigner);
-
-elementContainer* superParentPromote(int promotedValue, elementContainer*& superParent, int level, 
-							elementContainer** containerAssigner);
-							
-void insertValue(int value, elementContainer* destination, elementContainer*& parent, elementContainer** containerAssigner);
-
+struct element; struct elementContainer;
 
 //Element data struct contains the value of the element and the nested array that is associated with it
 struct element{
@@ -90,44 +63,22 @@ struct elementContainer{
 };
 
 
+elementContainer* promote(elementContainer* parent, int promotedValue, elementContainer*& superParent, int level,
+							elementContainer** containerAssigner);
+
+elementContainer* superParentPromote(int promotedValue, elementContainer*& superParent, int level, 
+							elementContainer** containerAssigner);
+							
+void insertValue(int value, elementContainer* destination, elementContainer*& parent, elementContainer** containerAssigner);
+
+
 //Find the location where an element should be inserted
-void findInsertLoc(int value, elementContainer*& superParent, elementContainer** containerAssigner){
-	
-	#ifdef DIAGNOSTICS
-	++g_measurements["inside findInsertLoc"];
-	#endif
-	
-	elementContainer* destination = superParent;
+elementContainer* findInsertLoc(int value, elementContainer* destination){
 	
 	//Loop until the lowest nest level is reached
-	#ifdef DIAGNOSTICS
-	while(++g_measurements["checking for bottom nest level"] && destination->nestedContainer != nullptr)
-	#else
-	while(destination->nestedContainer != nullptr)
-	#endif
-	{
-		#ifdef DIAGNOSTICS
-		if(destination->nestedContainer->parentContainer != destination){
-			printStructure(destination);
-			exit(0);
-		}
-		if(destination->element0.nestedContainer->parentContainer != destination){
-			printStructure(destination);
-			exit(0);
-		}
-		if(destination->hasTwoElements && destination->element1.nestedContainer->parentContainer != destination){
-			printStructure(destination);
-			exit(0);
-		}
-		#endif
+	while(destination->nestedContainer != nullptr){
 		
-		#ifdef DIAGNOSTICS
-		++g_measurements["if value >= destination->elements[0].val"];
-		#endif
 		if(value >= destination->element0.val){
-			#ifdef DIAGNOSTICS
-			++g_measurements["if !hasTwoElements ||"];
-			#endif
 			if(!destination->hasTwoElements || value < destination->element1.val){
 				//destination->elements[0].nestedContainer
 				destination = destination->element0.nestedContainer;
@@ -146,29 +97,16 @@ void findInsertLoc(int value, elementContainer*& superParent, elementContainer**
 		}
 	
 	}//End while loop
-	
-	insertValue(value, destination, superParent, containerAssigner);	//>:>Separate from find location function
-	
+
+	return destination;
 }
 
-//				>:<Explain why assignment happens here instead of inside promote
+//Insert a value into the bottom level
 void insertValue(int value, elementContainer* destination, elementContainer*& superParent, elementContainer** containerAssigner){
-	#ifdef DIAGNOSTICS
-	++g_measurements["insert into lowest level"];
-
-	if(destination->parentContainer == nullptr){
-		std::cout << "trying to insert value when parent container == nullptr";
-		exit(0);
-	}
-	if(destination->nestedContainer != nullptr){
-		std::cout << "trying to insert value when there is a nested container";
-		exit(0);
-	}
-	#endif
 	
 	//if the destination is full, promote
 	if(destination->hasTwoElements){
-		destination->hasTwoElements = false;	//an element will be promoted
+		destination->hasTwoElements = false;
 		elementContainer* newContainer;	//The container of the promoted element
 		
 		//promote the second highest value, assign the highest value as the element in the new container
@@ -219,21 +157,22 @@ void insertValue(int value, elementContainer* destination, elementContainer*& su
 }
 
 
-//Promote a value to its parent container, and assign the newly promoted value a nested container.
+//Promote a value to its parent container, and assign the promoted value a new nested container.
 //Return that nested container to the calling function to assign values to its own element and nested containers.
+//All values to the new container are assigned by the calling function, to avoid the need to check for the presence of nested arrays 
+	//(when insertValue calls a promotion it doesn't have nested containers which would otherwise be accessed). Only the parent has to be assigned here.
 elementContainer* promote(elementContainer* parent, int promotedValue, elementContainer*& superParent, int level,
 							elementContainer** containerAssigner){
 	
-	#ifdef DIAGNOSTICS
-	++g_measurements["inside promote"];
-	#endif
-	
+	//Trigger another promotion
 	if(parent->hasTwoElements){
-		parent->hasTwoElements = false;	//An element will be promoted
-		elementContainer* newContainer;	//The new container of the value that needs to be promoted (not to be confused with the 
-												//container this function will create and return) >:< better description
+		parent->hasTwoElements = false;
+		elementContainer* newContainer;	//The new container of the extra triggered promotion 
+											//(not to be confused with the container this function will create and return)
 		
+		//when there is no parent, super parent promote
 		if(parent->parentContainer == nullptr){
+			
 			//promote the second highest value to the new superparent, assign the highest value as the element in the new container
 			if(promotedValue >= parent->element0.val){
 				if(promotedValue >= parent->element1.val){
@@ -375,14 +314,11 @@ elementContainer* promote(elementContainer* parent, int promotedValue, elementCo
 	
 }
 
-//
+
+//Promote an element from the super parent, creating a new super parent on top of regular promotion
 elementContainer* superParentPromote(int promotedValue, elementContainer*& superParent, int level,
 							elementContainer** containerAssigner){
 	
-	#ifdef DIAGNOSTICS
-	++g_measurements["superparent promotion"];
-	#endif
-		
 	//Create the new super parent
 	elementContainer* newSuperParent = containerAssigner[level+1]++;
 	newSuperParent->hasTwoElements = false;
@@ -390,7 +326,6 @@ elementContainer* superParentPromote(int promotedValue, elementContainer*& super
 	newSuperParent->nestedContainer = superParent;
 	
 	superParent->parentContainer = newSuperParent;
-	superParent = newSuperParent;	//>:<moveable (to immediately before the return)
 	
 	newSuperParent->element0.val = promotedValue;
 	
@@ -398,33 +333,25 @@ elementContainer* superParentPromote(int promotedValue, elementContainer*& super
 	newSuperParent->element0.nestedContainer = containerAssigner[level]++;
 	newSuperParent->element0.nestedContainer->parentContainer = newSuperParent;
 	
+	superParent = newSuperParent;
 	return newSuperParent->element0.nestedContainer;
 }
 
 
-
-//>:>https://secweb.cs.odu.edu/~zeil/cs361/web/website/Lectures/recursionConversion/page/recursionConversion.html
 //Extract the elements from their nested elementContainers and put them in the correct, sorted order into a standard array
-void placeElementsInArray(elementContainer* source, int *array){
-	static int index = 0;//>:> more elegant method
-	
-	#ifdef DIAGNOSTICS
-	if(source->hasTwoElements){
-		++g_measurements["2 elements"];
-	}
-	#endif
+void placeElementsInArray(elementContainer* source, int* array, int& index){
 	
 	if(source->nestedContainer != nullptr){
-		placeElementsInArray(source->nestedContainer, array);
+		placeElementsInArray(source->nestedContainer, array, index);
 		
 		array[index] = source->element0.val;
 		++index;
-		placeElementsInArray(source->element0.nestedContainer, array);
+		placeElementsInArray(source->element0.nestedContainer, array, index);
 		
 		if(source->hasTwoElements){
 			array[index] = source->element1.val;
 			++index;
-			placeElementsInArray(source->element1.nestedContainer, array);
+			placeElementsInArray(source->element1.nestedContainer, array, index);
 		}
 	}
 	else{
@@ -451,63 +378,35 @@ void deallocateMemory(elementContainer*& allContainers){
 }
 
 
-#ifdef DIAGNOSTICS
-void printStructure(elementContainer* source, std::string containerIdentifier){
-	std::cout << "CONTAINER \"" << containerIdentifier << "\": ()";
-	std::cout << ", " << source->element0.val;
-	if(source->hasTwoElements){
-		std::cout << ", " << source->element1.val;
-	}
-	std::cout << "\n";
-	if(source->nestedContainer != nullptr){
-		printStructure(source->nestedContainer, containerIdentifier + "()");
-		printStructure(source->element0.nestedContainer, containerIdentifier + "0");
-		if(source->hasTwoElements){
-			printStructure(source->element1.nestedContainer, containerIdentifier + "1");
-		}
-	}
-}
-#endif
-
-void nestedArraySort(int *array, int arrayLength, elementContainer* containerAssigner){
+//Sort an array
+void pyramidSort(int *array, int arrayLength, elementContainer* containerMemory){
 	if(arrayLength <= 1)
 		return;
 	
-	elementContainer** contAssigner = new elementContainer*[(int)(std::log2(arrayLength + 1))];
+	elementContainer** containerAssigner = new elementContainer*[(int)(std::log2(arrayLength + 1))];
 	int position = arrayLength;
 	for(int i = 0; i < std::log2(arrayLength + 1);++i){
 		position /= 2;
-		contAssigner[i] = (containerAssigner + position);
+		containerAssigner[i] = (containerMemory + position);
 	}
-	
-	#ifdef DIAGNOSTICS
-	g_measurements["inside findInsertLoc"] = 0;
-	g_measurements["checking for bottom nest level"] = 0;
-	g_measurements["if value >= destination->elements[0].val"] = 0;	//(equivalent to the number of descents)
-	g_measurements["if !hasTwoElements ||"] = 0;
-	g_measurements["insert into lowest level"] = 0;
-	g_measurements["inside promote"] = 0;
-	g_measurements["superparent promotion"] = 0;
-	g_measurements["2 elements"] = 0;
-	#endif
 
 	//assign parent elementContainer
-	elementContainer* superParent = contAssigner[1]++;
+	elementContainer* superParent = containerAssigner[1]++;
 	
 	//initialize the first three containers (superParent and its 2 children)
 	superParent->hasTwoElements = false;
 	superParent->parentContainer = nullptr;
-	superParent->nestedContainer = contAssigner[0]++;
-	superParent->element0.nestedContainer = contAssigner[0]++;
+	superParent->nestedContainer = containerAssigner[0]++;
+	superParent->element0.nestedContainer = containerAssigner[0]++;
 	
 	superParent->nestedContainer->parentContainer = superParent;
 	superParent->nestedContainer->hasTwoElements = false;
-	//superParent->nestedContainer->element0.nestedContainer does not have to be set, it is never checked
-	superParent->nestedContainer->nestedContainer = nullptr;
 	
 	superParent->element0.nestedContainer->parentContainer = superParent;
 	superParent->element0.nestedContainer->hasTwoElements = false;
-	//superParent->element0.nestedContainer->element0.nestedContainer does not have to be set, it is never checked
+	
+	//Set the nested containers of both bottom containers to nullptr, as that is what is checked to determine if the bottom has been reached
+	superParent->nestedContainer->nestedContainer = nullptr;
 	superParent->element0.nestedContainer->nestedContainer = nullptr;
 	
 	
@@ -552,23 +451,19 @@ void nestedArraySort(int *array, int arrayLength, elementContainer* containerAss
 	
 	//Place the rest of the elements
 	for(int i = 3;i < arrayLength;++i){
-		findInsertLoc(array[i],superParent,contAssigner);
+		elementContainer* destination = findInsertLoc(array[i],superParent);
+		insertValue(array[i], destination, superParent, containerAssigner);
 	}
 	
-	
-	placeElementsInArray(superParent, array);
-	
-	#ifdef DIAGNOSTICS
-	for (std::map<std::string,int>::iterator it=g_measurements.begin(); it!=g_measurements.end(); ++it)
-		std::cout << it->first << " => " << it->second << '\n';
-	#endif
+	int index = 0;
+	placeElementsInArray(superParent, array, index);
 }
 
-
-void nestedArraySort(int* array, int arrayLength){
+//sort an array
+void pyramidSort(int* array, int arrayLength){
 	elementContainer* allContainers = nullptr;
 	allocateMemory(arrayLength,allContainers);
-	nestedArraySort(array, arrayLength, allContainers);
+	pyramidSort(array, arrayLength, allContainers);
 	deallocateMemory(allContainers);
 }
 
